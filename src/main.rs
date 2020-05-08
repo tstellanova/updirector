0bbdcfda41457814d04b19f37b9d38c7e6a48d13
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-05-08 16:20:10 -0700

    Moving toward signed req/resp

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use simple_logger::{self};
 
 use std::error::Error;
 
-use ruptane_common::{VehicleUpdateRequest, VehicleUpdateResponse};
+use ruptane_common::{SignedPayload, VehicleUpdateRequest, VehicleUpdateResponse, Signature};
 
-const LAMBDA_VERSION: u32 = 6;
+const LAMBDA_VERSION: u32 = 7;
 
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -18,15 +18,33 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn my_handler(e: VehicleUpdateRequest, c: lambda::Context) -> Result<VehicleUpdateResponse, HandlerError> {
-    if e.vehicle_id == "" {
-        error!("No vehicle_id {}", c.aws_request_id);
-        return Err(c.new_error("No vehicle_id in request"));
+fn my_handler(wrapper: SignedPayload<VehicleUpdateRequest>,
+              c: lambda::Context) -> Result<SignedPayload<VehicleUpdateResponse>, HandlerError> {
+    if wrapper.signatures.is_empty() {
+        error!("No signature {}", c.aws_request_id);
+        return Err(c.new_error("No signature in request"));
     }
 
-    Ok(VehicleUpdateResponse {
+    //TODO verify signature
+    let e = wrapper.signed;
+
+    //TODO do a lookup for this vehicle and generate a response
+    let raw_resp = VehicleUpdateResponse {
         resp_version: format!("{:04}",LAMBDA_VERSION),
         vehicle_id: e.vehicle_id,
         message: format!("Hello !"),
-    })
+    };
+
+    //TODO generate non-canned signatures
+    let sig = Signature {
+        keyid: "9a406d99e362e7c93e7acfe1e4d6585221315be817f350c026bbee84ada260da".into(),
+        method: "ed25519".into(),
+        sig: "335272f77357dc0e9f1b74d72eb500e4ff0f443f824b83405e2b21264778d1610e0a5f2663b90eda8ab05a28b5b64fc15514020985d8a93576fe33b287e1380f".into()
+    };
+    let wrapped_resp = SignedPayload::<VehicleUpdateResponse> {
+        signatures: vec![sig],
+        signed: raw_resp
+    };
+
+    Ok(wrapped_resp)
 }
